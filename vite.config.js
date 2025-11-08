a40d5cc7ae6b6af35731ff03cb0dca33d95661ec
PockeyBoss <juanrojas0407542@outlook.com>
2025-11-08 20:54:53 -0300

    modificaciones varias en configuraciones y dependencias

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Si usás dominio propio, esto evita rutas rotas
+  build: {
+    outDir: 'dist',
+  },
 });
