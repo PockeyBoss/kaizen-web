cf85388f9d77f7c87a538d646fe7bcb7664e1c4d
PockeyBoss <juanrojas0407542@outlook.com>
2025-11-08 21:57:00 -0300

    modified:   package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ IMPORTANTE: así debe estar para dominio personalizado
+  base: '/', // ✅ CORRECTO
   build: {
     outDir: 'dist',
   },
